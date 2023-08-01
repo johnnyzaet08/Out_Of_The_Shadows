2a8498c86e45c55c1743de09e9d9e9e23d00041f
JhonnASC <93461702+JhonnASC@users.noreply.github.com>
2023-07-31 22:25:51 -0600

    resources cambio fondo

--- src/components/BackgroundParallax.jsx
+++ src/components/BackgroundParallax.jsx
@@ -3,21 +3,11 @@ import image from '../images/BackgroundParallax.png';
 import ResourcesComponent from '../components/Resources';
 
 const ParallaxClouds = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollPosition(window.scrollY);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
     <div className="" style={{ 
-      backgroundPosition: `-${scrollPosition * 1}px 0`,
       backgroundImage: `url(${image})` }}>
       <ResourcesComponent />
     </div>
